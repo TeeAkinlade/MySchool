d35b4f6be704fc56ee19d1ac38bb465c5089d806
TeeAkinlade <taiye.akinlade!@gmail.com>
2024-05-23 21:53:04 +0100

    add the teacher sidebar

---
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -0,0 +1,9 @@
+const AnalyticsPage = () => {
+    return ( 
+        <div className="">
+            AnalyticsPage
+        </div>
+     );
+}
+ 
+export default AnalyticsPage;
---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,9 @@
+const CoursesPage = () => {
+    return ( 
+        <div className="">
+            CoursesPage
+        </div>
+     );
+}
+ 
+export default CoursesPage;
