3c89e676ae177dc96e23c316b4fc25157322718b
TeeAkinlade <taiye.akinlade!@gmail.com>
2024-08-03 22:54:36 +0100

    chnages to the shadcn popover and combobox files

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -47,27 +47,27 @@ export const Combobox = ({ options, value, onChange }: ComboBoxProps) => {
         <Command>
           <CommandInput placeholder="Search option..." />
           <CommandEmpty>No option found.</CommandEmpty>
-          <CommandList>
             <CommandGroup>
-              {options.map((option) => (
-                <CommandItem
-                  key={option.value}
-                  onSelect={() => {
-                    onChange(option.value === value ? "" : option.value)
-                    setOpen(false)
-                  }}
-                >
-                  <Check
-                    className={cn(
-                      "mr-2 h-4 w-4",
-                      value === option.value ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                  {option.label}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
+              <CommandList>
+                {options.map((option) => (
+                  <CommandItem
+                    key={option.value}
+                    onSelect={() => {
+                      onChange(option.value === value ? "" : option.value)
+                      setOpen(false)
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        value === option.value ? "opacity-100" : "opacity-0"
+                      )}
+                    />
+                    {option.label}
+                  </CommandItem>
+                ))}
+            </CommandList>
+          </CommandGroup>
          </Command>
       </PopoverContent>
     </Popover>
