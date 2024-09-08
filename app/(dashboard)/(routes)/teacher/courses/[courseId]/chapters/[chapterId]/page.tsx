4267aba0cf21843111f075d4b0151a8f37543cff
TeeAkinlade <taiye.akinlade!@gmail.com>
2024-09-08 18:25:35 +0100

    Chapter set up features

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,8 +1,12 @@
+import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import { ChapterTitleForm } from "./_component/ChapterTitleForm";
+import { ChapterAccess } from "./_component/ChapterAccess";
+import { ChapterDescriptionForm } from "./_component/ChapterDescriptionForm";
 
 const ChapterIdPage = async ({
   params
@@ -50,7 +54,53 @@ const ChapterIdPage = async ({
               <ArrowLeft className="h-4 w-4 mr-2" />
               Back to course setup
             </Link>
+            <div className="flex items-center justify-between w-full">
+              <div className="flex flex-col gap-y-2">
+                <h1 className="text-2xl font-medium">
+                  Chapter Creation
+                </h1>
+                <span className="text-sm text-slate-700">
+                  Complete all fields {completionText}
+                  </span>
+              </div>
+            </div>
         </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-12">
+        <div className="space-y-4">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={LayoutDashboard} />
+              <h2 className="text-xl">
+                Customize your chapter
+              </h2>
+            </div>
+            <ChapterTitleForm
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+            <ChapterDescriptionForm
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={Eye} />
+              <h2 className="text-xl">
+                 Access Settings
+              </h2>
+            </div>
+            <ChapterAccess
+              initialData={chapter}
+              courseId={params.courseId}
+              chapterId={params.chapterId}
+            />
+          </div>
+        </div>
+        <div></div>
       </div>
     </div>
   )
